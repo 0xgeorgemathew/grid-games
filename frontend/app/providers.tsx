4ba52baa652f2211c76ccba06945dad8c7392405
lizgeorge1 <lizmerin.george@gmail.com>
2026-02-05 06:30:50 +0530

    Privy auth added

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { PrivyProvider } from '@privy-io/react-auth'
+import { privyConfig } from '@/privy/config'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!} config={privyConfig}>
+      {children}
+    </PrivyProvider>
+  )
+}
